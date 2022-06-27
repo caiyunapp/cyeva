a71c2cd85572463f461891c7443ca7ab24bd1e0f
Wentao Li <clarmylee92510@gmail.com>
2022-06-27 17:09:10 +0800

    chore: Add black lint and adjust ci. (#15)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,7 @@ release = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme",
-    "sphinx.ext.mathjax"
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.mathjax"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
